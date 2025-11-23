304abd801497149df417f8a867f2a1f9ea5186c7
Kaeham <106434990+Kaeham@users.noreply.github.com>
2025-11-23 11:02:15 +1100

    added basic canvas and text representing kanji

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<style>
+	#kanjiCanvas {
+		border-color: black;
+        border-style: solid;
+	}
+    .text {
+        text-align: center;
+        align-items: center;
+    }
+</style>
+
+<h1>Kanji Writer</h1>
+<p> Practice your kanji writing here</p>
+<div>
+    <p class="text" id="kanjiKana"> kana goes here</p>
+    <canvas class="text" id="kanjiCanvas" width="500" height="300">
+        Your browser does not support canvas
+    </canvas>
+    <p class="text" id="kanjiMeaning"> kanji meaning goes here</p>
+</div>
