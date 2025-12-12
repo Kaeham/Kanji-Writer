9142cfbbcf547c6ad54ad1b3cf9b109536153d99
Kaeham <106434990+Kaeham@users.noreply.github.com>
2025-12-13 09:10:44 +1100

    Added kanjivg-js library

--- KanjiWriter/vite.config.ts
+++ KanjiWriter/vite.config.ts
@@ -1,6 +1,10 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { kvgJs } from 'kanjivg-js/vite-plugin';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+		sveltekit(),
+		kvgJs(),
+	]
 });
