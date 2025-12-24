5a8e3f392c44e6ce87c7bd4d19859d5b5b46a46d
Kaeham <106434990+Kaeham@users.noreply.github.com>
2025-12-25 10:54:55 +1100

    Added sampling for SVG Paths

--- KanjiWriter/src/lib/KanjiCanvas.svelte
+++ KanjiWriter/src/lib/KanjiCanvas.svelte
@@ -1,30 +1,26 @@
 <script lang="ts">
     import { onMount } from 'svelte';
-    import Kanji from './Kanji.svelte';
-    import { check_stroke_direction } from './CanvasMethods';
+    import * as kanji from './Kanji.ts';
+    import type { StrokeData } from 'kanjivg-js';
 
+    let contentDiv:HTMLDivElement;
+    
+    // drawing
+    import { check_stroke_direction, parse_svg_line, render_svg_line, draw, render_sampled_points } from './CanvasMethods';
     let drawingCanvas:HTMLCanvasElement;
     let displayCanvas:HTMLCanvasElement;
     let drawingCtx:CanvasRenderingContext2D;
     let displayCtx:CanvasRenderingContext2D;
     let flag = false;
     let dot_flag = false
-    const stroke_direction: number = 0;
-    const stroke_coords: [[number, number], [number, number]] = [[100, 100], [200, 100]];
-    let chars:string[];
-    let meaning:string; 
-    let kana:string[];
 
-    onMount(() => { 
-        drawingCtx = drawingCanvas.getContext("2d")!;
-        displayCtx = displayCanvas.getContext("2d")!;        
-    })
-
-
+    let currentStroke:number = 0;
+    let currentCharacter:number = 0;
+    let stroke_direction:number;
+    let stroke_coords:[number, number][] = [];
     // curr and prev is for drawing the stroke
     let prevX = 0, prevY = 0;
     let currX = 0, currY = 0;
-    
     // Start & Final is for calculations for a single stroke
     let startX = 0, startY = 0;
     let finalX = 0, finalY = 0;
@@ -32,15 +28,7 @@
 
     const strokeColor = "black", strokeWidth = 5;
 
-    function draw(ctx:CanvasRenderingContext2D, x1:number, x2:number, y1:number, y2:number) {
-        ctx.beginPath();
-        ctx.moveTo(x1, y1);
-        ctx.lineTo(x2, y2);
-        ctx.strokeStyle = strokeColor;
-        ctx.lineWidth = strokeWidth;
-        ctx.stroke();
-        ctx.closePath();
-    }
+    
     function HandleDown(e:MouseEvent) {
         prevX = currX;
         prevY = currY;
@@ -48,6 +36,8 @@
         currY = e.clientY - drawingCanvas.offsetTop;
         startX = currX;
         startY = currY;
+        let stroke_coords = [];
+        stroke_coords.push([[startX, startY]])
         // console.log("Down, prev:", prevX, "cur", currX)
         flag = true;
         dot_flag = true;
@@ -60,7 +50,10 @@
             }
     }
     function HandleOff(e:MouseEvent) {
-        flag = false;
+        if (flag === true) {    
+            flag = false;
+            HandleUp(e)
+        }
     }
     function HandleMove(e: MouseEvent) {
         if (flag) {
@@ -68,18 +61,28 @@
             prevY = currY;
             currX = e.clientX - drawingCanvas.offsetLeft;
             currY = e.clientY - drawingCanvas.offsetTop;
-            draw(drawingCtx,prevX, currX, prevY, currY);
+            stroke_coords!.push([currX, currY])
+            draw(drawingCtx, prevX, currX, prevY, currY);
             }
     }
     function HandleUp(e: MouseEvent) {
+        flag = false;
         finalX = currX;
         finalY = currY;
-        flag = false;
+        stroke_coords!.push([finalX, finalY])
+        
         let dx = finalX - startX;
         let dy = startY - finalY; // Y is 0 at the top for some reason?
+        let count:number = 0;
         stroke = check_stroke_direction(dx, dy);
         console.log("stroke dir: ", stroke);
         drawingCtx.clearRect(0, 0, drawingCanvas.width, drawingCanvas.height)
+        console.log(stroke_coords)
+        stroke_coords!.forEach((element:number[]) => {
+            let x, y = element;
+
+        });
+        stroke_coords = []; // clear coordinates
         if (stroke === stroke_direction) {
             // add a length/width checker for the stroke
             let [[x1, y1], [x2, y2]] = stroke_coords;
@@ -90,40 +93,57 @@
         }
     }
 
+    // kanji info
+    let characters: string[];
+    let strokeData: StrokeData[][];
+    let unicodes;
 
-
+    onMount( async () => { 
+        drawingCtx = drawingCanvas.getContext("2d")!;
+        displayCtx = displayCanvas.getContext("2d")!;
+        let word:string = "高校";
+        await kanji.extractKanjiInfo(word);
+        characters = kanji.getCharacters();
+        strokeData = kanji.getStrokeData();
+        unicodes = kanji.getUnicode();
+        // console.log(characters)
+        // console.log(strokeData[currentCharacter][currentStroke])
+        let points = parse_svg_line(unicodes[currentCharacter], strokeData[currentCharacter][currentStroke].path);
+        render_sampled_points(drawingCtx, points)
+        render_svg_line(displayCtx, strokeData[currentCharacter][currentStroke].path, drawingCtx)
+    })
 </script>
 
-<Kanji kanji="高"/>
+<div class="KanjiCanvas" bind:this={contentDiv}>
 
-<canvas bind:this={drawingCanvas}
+    <canvas bind:this={drawingCanvas}
     onmousedown={HandleDown}
     onpointerup={HandleUp}
     onmouseout={HandleOff}
     onblur={null}
     onmousemove={HandleMove}
-    width=500
+    width=300
     height=300
     id="kanjiDrawingCanvas"
-></canvas>
+    ></canvas>
 
-<button onclick={() => drawingCtx.clearRect(0, 0, drawingCanvas
-    .width, drawingCanvas.height)}>
-    Clear
-</button>
-
-<canvas 
+    <canvas 
     bind:this={displayCanvas}
-    width=500
+    width=300
     height=300
     id="kanjiDisplayCanvas">
-
-</canvas>
+    </canvas>
+</div>
 
 
 <style>
     #kanjiDrawingCanvas {
-		border-color: black;
+        border-color: black;
         border-style: solid;
 	}
+
+    .KanjiCanvas {
+        border-color: red;
+        border-style: solid;
+    }
 </style>
