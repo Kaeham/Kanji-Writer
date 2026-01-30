02665671d18795ba4478462b2624dde09cc9bb32
Kaeham <106434990+Kaeham@users.noreply.github.com>
2026-01-31 09:23:08 +1100

    Added temporary navbar

--- KanjiWriter/src/routes/+layout.svelte
+++ KanjiWriter/src/routes/+layout.svelte
@@ -8,5 +8,28 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
+<nav>
+	<!-- <ul class="navbar"></ul>
+		<li class="navItem"> <a href="/"> Home</a></li>
+		<li class="navItem"> <a href="/deck"> Deck</a></li>
+		<li class="navItem"> <a href="/review"> Review</a></li>
+		<li class="navItem"> <a href="/test"> Test</a></li>
+	</ul> -->
+	<a href="/"> Home</a>
+	<a href="/deck"> Deck</a>
+	<a href="/review"> review</a>
+	<a href="/test"> test</a>
+</nav>
+
+<style>
+	.navbar {
+		display: flex;
+		flex-direction: row;
+	}
+	.navItem {
+		padding-right: 20px;
+		
+	}
+</style>
 
 {@render children()}
