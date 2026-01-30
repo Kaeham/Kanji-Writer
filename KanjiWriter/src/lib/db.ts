6b6795e581f562176c176ebe7867752cc95caaa5
Kaeham <106434990+Kaeham@users.noreply.github.com>
2026-01-31 10:39:46 +1100

    Added deck 'table' and functions

--- KanjiWriter/src/lib/db.ts
+++ KanjiWriter/src/lib/db.ts
@@ -13,15 +13,16 @@ function preprocess_kanji(input_kanji:string): AddDatabaseOperation {
     return { ok: true}
 }
 
-db.version(1).stores({
-    cards: '++id, &kanji_id, lastReviewDate, dueReviewDate',
+db.version(2).stores({
+    deck: '++id, &name, card_count',
+    cards: '++id, kanji_id, deck, lastReviewDate, dueReviewDate',
     kanji: '++id, &kanji',
 })
 
 
 export async function add_kanji(input_kanji: string): Promise<AddDatabaseOperation> {
-    if (!input_kanji) {return {error: "empty input"}}
-    if (nonJapaneseRegex.test(input_kanji)) {return {error: "non japanese characters found!"}}
+    const preprocess = preprocess_kanji(input_kanji)
+    if (!preprocess.ok) {return preprocess}
     try {
         await db.table("kanji").add({kanji: input_kanji})
         return { ok: true}
@@ -43,31 +44,76 @@ async function get_kanji(kanji:string) {
         .equals(kanji)
         .first();
     } catch (err) {
-        console.error("Dexie get_kanji failed:", err);
+        console.error("Dexie get_kanji failed: ", err);
         return undefined
     }
 }
 
-export async function add_card(input_kanji:string): Promise<AddDatabaseOperation> {
+export async function add_card(input_kanji:string, deck_name:string): Promise<AddDatabaseOperation> {
     const preprocess = preprocess_kanji(input_kanji)
     if (!preprocess.ok) {return preprocess}
     try {
-        let kanji = await get_kanji(input_kanji);
-        if (!kanji) {
-            const addKanji = await add_kanji(input_kanji)
-            if (!addKanji.ok) {return addKanji}
-            kanji = await get_kanji(input_kanji);
-        }
+        await db.transaction("rw", 
+            db.table("kanji"), db.table("cards"), db.table("deck"),
+        async () => {
+            const deck = get_deck(deck_name)
+            if (!deck) {throw new Error("DECK_NOT_FOUND")}
+            let kanji = await get_kanji(input_kanji);
+            if (!kanji) {
+                const addKanji = await add_kanji(input_kanji)
+                if (!addKanji.ok) {throw new Error("KANJI_CREATION_FAILED");}
+                kanji = await get_kanji(input_kanji);}
+            
+            if (!kanji) {throw new Error("KANJI_CREATION_FAILED")}
+            
+            await db.table("cards").add({
+                kanji_id: kanji.id,
+                deck: deck_name,
+                lastReviewDate: undefined,
+                dueReviewDate: Date.now()});
+            
+            await db
+            .table("deck")
+            .update(deck.id, {card_count: deck.card_count + 1})
+        });
 
-        await db.table("cards").add({
-            kanji_id: kanji.id,
-            lastReviewDate: undefined,
-            dueReviewDate: Date.now()});
-            return { ok: true };
+        return { ok: true };
     } catch (err:any ) {
         if (err?.name === "ConstraintError") {
             return { error: "card already exists" };
         }
+        if (err?.name === "DECK_NOT_FOUND") {return {error: "deck does not exist"}}
+        if (err?.name === "KANJI_CREATION_FAILED") {return {error: "kanji failed to be added"}}
         console.error("add card failed", err);
         return { error: "database error"}
     }}
+
+export async function get_deck(name:string) {
+    if (!name) {return undefined}
+    try {
+        return await db
+        .table("deck")
+        .where("name")
+        .equals(name)
+        .first();
+    } catch (err) {
+        console.error("Dexie get_deck failed: ", err)
+        return undefined
+    }
+}
+
+export async function init_deck(name:string):Promise<AddDatabaseOperation> {
+    if (!name) {return {error: "empty name is not allowed"}}
+    const deck = await get_deck(name)
+    if (deck) {return {error: "a deck with that name already exists"}}
+
+    try {
+        await db.table("deck").add({
+            name: name,
+            card_count: 0})
+            return {ok: true}
+    } catch (err:any) {
+        console.error("add deck failed", err)
+        return {error: "database error"}
+    }
+}
