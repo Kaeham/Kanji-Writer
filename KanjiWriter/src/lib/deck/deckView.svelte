04233f1352c4328dd6d8d1199fe13403ed70650e
Kaeham <106434990+Kaeham@users.noreply.github.com>
2026-02-06 10:00:41 +1100

    Added viewing for cards in a deck

--- KanjiWriter/src/lib/deck/deckView.svelte
+++ KanjiWriter/src/lib/deck/deckView.svelte
@@ -1,22 +1,80 @@
 <script>
+    // @ts-nocheck
     import { get_deck } from "$lib/db";
-    import { onMount } from "svelte";
+    import { getAbortSignal, onMount } from "svelte";
+    import { get_all_cards } from "$lib/card/cardDBFunctions";
 
     let {deck} = $props()
+    $inspect(deck).with(console.log)
+    let cards = $state([])
 
+    function preprocess_card(card) {
+
+    }
+
+    $effect(() => {
+        const deckName = deck.name
+        get_all_cards(deckName).then(allCards => {
+            cards = allCards;
+            console.log(cards)
+        });
+    })
 </script>
 
 
 <div class="deck">
     <div class="deckTitles">
-        <p>name: {deck.name}</p>
-        <p>count: {deck.card_count}</p>
+        <p>{deck.name}</p>
     </div>
     <div class="deckContent">
-        <!-- {#each deck as element}
-        <p>{element}</p>
-        {/each} -->
+        <div class="deckTitles">
+            <p class="tableTitles">Kanji</p>
+            <p class="tableTitles">Deck</p>
+            <p class="tableTitles">Review Date</p>
+        </div>
+        {#each cards as card}
+        <div class="tableRows">
+            <p class="rowContent">{card.kanji_id}</p>    
+            <p class="rowContent">{card.deck}</p>    
+            <p class="rowContent">{card.dueReviewDate}</p>
+        </div>
+        {/each}
     </div>
 </div>
 
-<style></style>
+<style>
+    .deckTitles {
+        border: 1px;
+        border-style: solid;
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+    }
+
+    .tableTitles {
+        border: 0px 0px 0px 0px;
+        border-style: solid;
+        height: 100%;
+        width: 100%;
+        display: inherit;
+        justify-content: center;
+    }
+
+    .tableContent {
+        
+    }
+    .tableRows {
+        display: flex;
+        flex-direction: row;
+    }
+
+    .rowContent {
+        border: 1px;
+        border-style: solid;
+        border-color: gray;
+        border-radius: 0px;
+        width: 100%;
+        padding: 5px 0px 5px 5px;
+    }
+
+</style>
