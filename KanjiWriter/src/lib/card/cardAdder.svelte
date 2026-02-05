d8581405a4bc6f5a0a162bda69a0b121baea91d6
Kaeham <106434990+Kaeham@users.noreply.github.com>
2026-02-06 09:12:43 +1100

    Added adder component for cards

---
+++ KanjiWriter/src/lib/card/cardAdder.svelte
@@ -0,0 +1,43 @@
+<script>
+    // @ts-nocheck
+    import { db, get_kanji, add_card } from "$lib/db";
+    import DeckSelector from "$lib/deck/deckSelector.svelte";
+    let inputKanji = $state("");
+    let dbKanji = $state("");
+    let submitButton;
+    
+    let selectedDeck = $state(null);
+
+    function deckHandle(deck) {
+        selectedDeck = deck;
+        console.log("selected deck:", selectedDeck)
+    }
+    
+    $effect(() => {
+        if (!inputKanji) {
+            dbKanji = "";
+            return;}
+        get_kanji(inputKanji).then(result => {
+        dbKanji = result?.kanji ?? "Not Found"});
+    });
+
+</script>
+
+
+<div>
+    <p class="page">Add Card</p>
+    <input type="input" bind:value={inputKanji}>
+    <DeckSelector onSelect={deckHandle}/>
+    <button bind:this={submitButton} onclick={() => add_card(inputKanji, selectedDeck)}>Submit</button>
+    <!-- <button onclick={showdb}>Show</button>
+    <button onclick={() => get_kanji(inputKanji)}>Check Kanji</button> -->
+</div>
+
+<style>
+    .page {
+        background-color: darkslategrey;
+        width: 100%;
+        height: 100%;
+        display: block;
+    }
+</style>
