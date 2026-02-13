0602715645f42a8a7416c9c622c53852b27c4972
Kaeham <106434990+Kaeham@users.noreply.github.com>
2026-02-14 09:56:33 +1100

    Modularised db logic

--- KanjiWriter/src/lib/db.ts
+++ KanjiWriter/src/lib/db.ts
@@ -20,36 +20,3 @@ db.version(3).stores({
     cards: '++id, &kanji_id, front, back, deck, lastReviewDate, dueReviewDate, dateAdded. dateUpdated',
     kanji: '++id, &kanji, kana, meaning, dateAdded, dateUpdated',
 })
-
-export async function get_deck(name:string) {
-    if (!name) {return undefined}
-    if (name === "*") {return await db.table("deck").toArray()}
-    try {
-        return await db
-        .table("deck")
-        .where("name")
-        .equals(name)
-        .first();
-    } catch (err) {
-        console.error("Dexie get_deck failed: ", err)
-        return undefined
-    }
-}
-
-export async function init_deck(name:string) {
-    if (!name) {return {error: "empty name is not allowed"}}
-    const deck = await get_deck(name)
-    if (deck) {return {error: "a deck with that name already exists"}}
-    return db.transaction("rw", db.table("deck"),
-        async () => {
-            await db.table("deck").add({
-                name: name,
-                card_count: 0})          
-            }).then(() => {
-                console.log("Transaction commited for deck creation")
-                return {ok: true};
-            }).catch(err => {
-                console.error("add deck failed", err)
-                return {error: "database error"}
-            });
-}
